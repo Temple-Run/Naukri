1702493b38653155c4d52d8521615643f5c9a01c
Temple-Run <73884364+Temple-Run@users.noreply.github.com>
2020-12-11 16:12:13 +0530

    Initial commit

---
+++ Naukri_dailyjob/src/main/java/PageClasses/Login.java
@@ -0,0 +1,43 @@
+package PageClasses;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Login {
+WebDriver driver;
+	
+	public void LoginMethod() throws InterruptedException {
+		
+		iniatingloginpage();
+		enteringcredentials();
+		 logout();
+		
+	}
+	public void iniatingloginpage() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\divya.a.murugesan\\Documents\\Drivers\\chromedriver.exe");
+		driver= new ChromeDriver();
+		driver.get("https://www.naukri.com/nlogin/login");
+		driver.manage().window().maximize();
+		System.out.println("iniatingloginpage");
+		
+	}
+	public void enteringcredentials() throws InterruptedException {
+		
+		driver.findElement(By.id("usernameField")).sendKeys("mdivya290997@gmail.com");
+		driver.findElement(By.id("passwordField")).sendKeys("#Diya2909#");
+		driver.findElement(By.xpath("//button[text()='Login']")).click();
+		Thread.sleep(10000);
+		System.out.println("enteringcredentials");
+	}
+	public void logout() throws InterruptedException {
+		Actions actions = new Actions(driver);
+		actions.moveToElement(driver.findElement(By.xpath("//div[text()='My Naukri']/parent::a"))).perform();;
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//a[text()='Logout']")).click();
+		
+		System.out.println("logout");
+	}
+	
+}
---
+++ Naukri_dailyjob/src/test/java/Mainclass/Dailyjob_naukri.java
@@ -0,0 +1,14 @@
+package Mainclass;
+
+import PageClasses.Login;
+
+public class Dailyjob_naukri extends Login {
+
+	public static void main(String[] args) throws InterruptedException {
+	
+		Login logn = new Login();
+		logn.LoginMethod();
+	}
+	
+	
+}
